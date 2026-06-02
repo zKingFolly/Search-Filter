b72482409cf8781fc7d6ba2d3d3c181af291f2db
Adefolarin <adefolaringithub@gmail.com>
2026-06-03 00:24:17 +0100

    Done with search logic

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 const search = document.getElementById("search");
 const addbox = document.getElementById("addInput");
 const itemBox = document.getElementById("item-box");
-let list = ["Bread", "milk", "Hypo"];
+let liElements = document.querySelectorAll("li");
+
+let list = ["Bread", "Milk", "Egg", "Oat"];
+list.forEach((value) => {
+  itemBox.innerHTML += `<li> ${value}</li>`;
+});
 function add() {
   list.push(addbox.value);
   update();
   addbox.value = "";
 }
-list.forEach((value) => {
-  itemBox.innerHTML += `<li> ${value}</li>`;
-});
 function update() {
   itemBox.innerHTML = ``;
   list.forEach((value) => {
     itemBox.innerHTML += `<li> ${value}</li>`;
   });
 }
+search.addEventListener("keyup", (e) => {
+  searchContent = e.target.value.toLowerCase();
+  itemBox.innerHTML = "";
+  list.forEach((value, index) => {
+    if (value.toLowerCase().includes(searchContent)) {
+      itemBox.innerHTML += `<li> ${value}</li>`;
+    }
+  });
+});
