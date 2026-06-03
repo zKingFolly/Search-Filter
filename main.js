c07b1fa98dfe8aa4d94bdfe61a4b52d79c12e407
Adefolarin <adefolaringithub@gmail.com>
2026-06-03 02:17:06 +0100

    Added delete functionality to wrap it up

--- main.js
+++ main.js
@@ -2,28 +2,45 @@ const search = document.getElementById("search");
 const addbox = document.getElementById("addInput");
 const itemBox = document.getElementById("item-box");
 let liElements = document.querySelectorAll("li");
-
 let list = ["Bread", "Milk", "Egg", "Oat"];
-list.forEach((value) => {
-  itemBox.innerHTML += `<li> ${value}</li>`;
-});
-function add() {
-  list.push(addbox.value);
-  update();
-  addbox.value = "";
-}
+
 function update() {
   itemBox.innerHTML = ``;
-  list.forEach((value) => {
-    itemBox.innerHTML += `<li> ${value}</li>`;
+  list.forEach((value, index) => {
+    itemBox.innerHTML += `<li> ${value} <button i="${index}">DELETE</button></li>`;
   });
 }
+update();
+
+function add() {
+  if (addbox.value.trim() !== "") {
+    list.push(addbox.value);
+    update();
+    addbox.value = "";
+  }
+}
+
 search.addEventListener("keyup", (e) => {
+  let isfound = false;
   searchContent = e.target.value.toLowerCase();
   itemBox.innerHTML = "";
   list.forEach((value, index) => {
     if (value.toLowerCase().includes(searchContent)) {
-      itemBox.innerHTML += `<li> ${value}</li>`;
+      itemBox.innerHTML += `<li> ${value} <button i="${index}">DELETE</button></li>`;
+      isfound = true;
     }
   });
+  if (!isfound) {
+    itemBox.textContent = `No such item`;
+  }
+});
+
+itemBox.addEventListener("click", (e) => {
+  if (e.target.tagName == "BUTTON") {
+    let elibtn = e.target.getAttribute("i");
+    if (elibtn !== null) {
+      list.splice(elibtn, 1);
+      update();
+    }
+  }
 });
